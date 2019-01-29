94f16746c43172b5915d04c987bdbbde894925e8
kupec <kupec-k@ya.ru>
2019-01-29 21:47:47 +0300

    Make install.sh idempotent

--- install.sh
+++ install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+function npm-install {
+	if ! which "$1" >/dev/null; then
+		npm i -g "$1"
+	fi;
+}
+
+ROOTDIR="~/.vim"
+BUNDLEDIR="$ROOTDIR/bundle"
+
 cat > ~/.vimrc <<EOF
-source ~/.vim/vimrc
+source $ROOTDIR/vimrc
 EOF
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ -d "$BUNDLEDIR/Vundle.vim" ]]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git "$BUNDLEDIR/Vundle.vim"
+fi;
+
+if ! which npm >/dev/null; then
+	echo "[error]: Please install npm" >&2
+	exit 1;
+fi;
 
 # vim-livedown (markdown live)
-npm i -g livedown
+npm-install livedown
 
 # vim-prettier
-npm i -g prettier
+npm-install prettier
 
 vim +PluginInstall +qall

6a95e8df4b34f98f6438c644126efe88f9f4ac6f
Dmitry Kostromin <kupec-k@ya.ru>
2022-01-03 14:46:24 +0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
-set -ex
+set -e
 
 function is-ubuntu {
-	which apt-get;
+    which apt-get >/dev/null;
 }
 
 function is-macos {
-	which port;
+    which port >/dev/null;
 }
 
 function pip-install {
-	if ! which "$1" >/dev/null; then
-		sudo python3 -m pip install --user "$1"
-	fi;
+    NOT_INSTALLED_PACKAGES=()
+    for package; do
+        if ! "$PYTHON3" -m pip show "$package" >/dev/null; then
+            NOT_INSTALLED_PACKAGES+=("$package")
+        fi;
+    done;
+
+    if [[ ${#NOT_INSTALLED_PACKAGES[@]} -gt 0 ]]; then
+        echo "Installing python packages: ${NOT_INSTALLED_PACKAGES[@]}"
+        "$PYTHON3" -m pip install --user "${NOT_INSTALLED_PACKAGES[@]}"
+    else
+        echo "All python packages are installed already"
+    fi;
 }
 
-function package-install {
-	if is-ubuntu; then
-		local NEED_INSTALL=""
-		for x; do
-			if ! dpkg-query -s $x >/dev/null; then
-				NEED_INSTALL=yes
-				break
-			fi;
-		done;
+function packages-install {
+    if is-ubuntu; then
+        local NEED_INSTALL=""
+        for package; do
+            if ! dpkg-query -s "$package" >/dev/null; then
+                NEED_INSTALL=yes
+                break
+            fi;
+        done;
 
-		if [[ $NEED_INSTALL == "yes" ]]; then
-			sudo apt install "$@" -y
-		fi;
-	elif is-macos; then
-		sudo port install "$@"
-	fi;
-	
+        if [[ $NEED_INSTALL == "yes" ]]; then
+            sudo apt update
+            sudo apt install "$@" -y
+        else
+            echo "All system packages are installed already"
+        fi;
+    elif is-macos; then
+        sudo port install "$@"
+    fi;
+    
 }
-
-if is-ubuntu; then
-	sudo apt update
-fi;
 
 PYTHON3=python3
 PIP3=python3-pip
@@ -45,25 +54,28 @@ if is-macos; then
     PIP3=py39-pip
 fi;
 
-package-install git curl xsel $PYTHON3 $PIP3
+PACKAGES=(git curl xsel "$PYTHON3" "$PIP3")
 # check if npm installed from other sources first (download manually, for example)
-which npm || package-install npm
+which npm >/dev/null || PACKAGES+=('npm')
+
+# fd (fzf)
+if is-ubuntu; then
+    FD_PACKAGE=fd-find
+else
+    FD_PACKAGE=fd
+fi;
+
+PACKAGES+=("$FD_PACKAGE" ripgrep watchman)
+
+packages-install "${PACKAGES[@]}"
 
 # add coc extensions
+echo "Installing coc.nvim extensions"
 (
 mkdir -p "$HOME/.config/coc/extensions";
 cd "$HOME/.config/coc/extensions";
 npm install;
 )
-
-# fd (fzf)
-if is-ubuntu; then
-	FD_PACKAGE=fd-find
-else
-	FD_PACKAGE=fd
-fi;
-
-package-install "$FD_PACKAGE" ripgrep watchman
 
 # Install neovim python modules + plugin modules
 pip-install pynvim flake8 autopep8 isort jedi
@@ -76,39 +88,48 @@ if is-ubuntu; then
     NVIM_APPIMAGE_DESC_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
     NVIM_APPIMAGE_URL="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
 
-    NVIM_NEW_VERSION=$(curl "$NVIM_APPIMAGE_DESC_URL" \
+    NVIM_NEW_VERSION=$(curl -s "$NVIM_APPIMAGE_DESC_URL" \
         | "$PYTHON3" -c 'import sys; import json; print(json.load(sys.stdin)["html_url"])')
     touch "$NVIM_VERSION_FILE"
     NVIM_CUR_VERSION=$(<"$NVIM_VERSION_FILE")
 
     if [[ $NVIM_NEW_VERSION != $NVIM_CUR_VERSION ]]; then
+        echo "Downloading new version of neovim"
         wget "$NVIM_APPIMAGE_URL" -O "$NVIM"
         chmod +x "$NVIM"
 
         echo $NVIM_NEW_VERSION > $NVIM_VERSION_FILE
+    else
+        echo "Installed neovim version is already latest"
     fi;
 
-	# Install neovim gui wrapper
-	if [[ ! -d neovim-gnome-terminal ]]; then
-	  (
-	  git clone https://github.com/fmoralesc/neovim-gnome-terminal-wrapper.git neovim-gnome-terminal;
-	  cd neovim-gnome-terminal;
-	  sudo cp nvim-wrapper /usr/bin/nvim-wrapper;
-	  sudo cp neovim.desktop /usr/share/applications/neovim.desktop;
-	  sudo cp neovim.svg /usr/share/icons/neovim.svg;
-	  )
-	fi;
+    # Install neovim gui wrapper
+    if [[ ! -d neovim-gnome-terminal ]]; then
+        echo "Installing neovim UI"
+        (
+        git clone https://github.com/fmoralesc/neovim-gnome-terminal-wrapper.git neovim-gnome-terminal;
+        cd neovim-gnome-terminal;
+        sudo cp nvim-wrapper /usr/bin/nvim-wrapper;
+        sudo cp neovim.desktop /usr/share/applications/neovim.desktop;
+        sudo cp neovim.svg /usr/share/icons/neovim.svg;
+        )
+    fi;
 fi;
 
 NVIM_DATA_DIR=$("$NVIM" -u NONE --headless -c 'echo stdpath("data") | quitall' 2>&1)
 NVIM_PACKER="$NVIM_DATA_DIR/site/pack/packer/start/packer.nvim"
 
 if [[ ! -d "$NVIM_PACKER" ]]; then
+    echo "Installing nvim packer (plugin manager)"
     git clone --depth 1 \
         https://github.com/wbthomason/packer.nvim \
         "$NVIM_PACKER"
+else 
+    echo "Plugin manager is already installed"
 fi;
 
-NVIM_INSTALL_PLUGIN_MODE=yes "$NVIM" --headless
+echo "Installing/updating nvim plugins"
+NVIM_INSTALL_PLUGIN_MODE=yes "$NVIM" --headless; echo
+echo "Installing coc.nvim extensions"
 "$NVIM" --headless -c 'CocInstall -sync coc-tsserver coc-css coc-json coc-html coc-python | quitall'
 

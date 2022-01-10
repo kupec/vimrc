c4c0dfaae1ff5037895ef20513eb1b0213fa2394
Dmitry Kostromin <d.kostromin@corp.vk.com>
2022-01-10 10:11:46 +0300

    Fix up open project win height

--- lua/project/init.lua
+++ lua/project/init.lua
@@ -14,12 +14,13 @@ local possible_projects_dirs = {"~/proj", "~/projects"}
 function E.open_project(path)
     vim.cmd('tcd ' .. path)
     vim.cmd 'new'
+    vim.cmd 'execute "normal \\<c-w>o"'
 
     local win_width = vim.api.nvim_win_get_width(0)
     local win_height = vim.api.nvim_win_get_height(0)
 
     local valign = {}
-    for i=1, win_height / 2 - 1 do
+    for i=1, win_height / 3 - 1 do
         valign[i] = ''
     end
     vim.api.nvim_buf_set_lines(0, 0, 0, true, valign)
@@ -31,8 +32,6 @@ function E.open_project(path)
     vim.cmd('$-2,$center ' .. win_width)
     vim.bo.modified = false
     vim.bo.modifiable = false
-
-    vim.cmd 'execute "normal \\<c-w>o"'
 end
 
 function E.open_project_in_new_tab(path)

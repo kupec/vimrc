01e0aa53d24f86e1243784288f3a0c255696ff02
Dmitry Kostromin <d.kostromin@corp.vk.com>
2022-02-04 17:57:39 +0300

    Remove freezes from autocompletion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,13 +50,10 @@ require('packer').startup(function(use, use_rocks)
     use {
         'hrsh7th/nvim-cmp',
         requires = {
-            'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-nvim-lua',
             'quangnguyen30192/cmp-nvim-ultisnips',
-            'octaltree/cmp-look',
-            'f3fora/cmp-spell',
         }
     }
 
--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,18 +1,13 @@
 local cmp = require('cmp')
 
---vim.cmd 'set completeopt=menu,menuone,noselect'
-
 cmp.setup {
     formatting = {
         format = function(entry, vim_item)
             vim_item.menu = ({
-                buffer = "[Buffer]",
                 nvim_lsp = "[LSP]",
                 ultisnips = "[UltiSnips]",
                 nvim_lua = "[Lua]",
-                look = "[Look]",
                 path = "[Path]",
-                spell = "[Spell]",
             })[entry.source.name]
             return vim_item
         end
@@ -35,13 +30,10 @@ cmp.setup {
         end
     },
     sources = {
-        {name = 'buffer'},
         {name = "path"},
         {name = 'nvim_lsp'},
         {name = "nvim_lua"},
         {name = "ultisnips"},
-        {name = "look"},
-        {name = "spell"},
     },
     completion = {completeopt = 'menu,menuone,noselect'}
 }
